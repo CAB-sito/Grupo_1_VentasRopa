6fb00c9d5e1dcacc7c5e534482ecac5ac76e6857
Brian <carabajalbrianagustin2@gmail.com>
2023-11-06 15:20:59 -0300

    cracion de estructura de archivos

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const paht = require('path');
+
+app.use(express.static('public'));
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+app.get("/",(req, res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/index.html"));
+});
---
+++ retro.md
@@ -0,0 +1,14 @@
+METODOLOGIA ESTRELLA:
+1.Comenzar a hacer
+2.hacer mas
+3.continuar haciendo
+4.hacer menos
+5.dejar de hacer
+
+
+
+1.Reuniones diarias. 
+2.Mas paricipacion de cada miembro.
+3.Distribucion de tarea de forma equitativa.
+4.
+5.
