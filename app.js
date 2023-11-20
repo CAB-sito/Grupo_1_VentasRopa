ffd5d749c1f70987e0f38575c265a16322e3ceec
Cecilia <cast.cv@gmail.com>
2023-11-20 11:45:04 -0300

    agrego productCart

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const paht = require('path');
+const paht = require("path");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+app.listen(3000, () => {
+  console.log("Servidor funcionando");
 });
-app.get("/",(req, res)=>{
-    res.sendFile(path.resolve(__dirname, "./views/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/index.html"));
+});
+app.get("/productCart.html", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/productCart.html"));
 });
