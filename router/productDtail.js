f9fe6c3112fbeb8b021b2b74bb0b6bd16dd04553
Cecilia <cast.cv@gmail.com>
2023-12-05 19:39:21 -0300

    Eliminado routers inutilizados

--- router/productDtail.js
+++
@@ -1,8 +0,0 @@
-const controllers = require("../controllers/productDtailController");
-const express = require('express');
-const router = express.Router();
-
-
-router.get('/productDtail', controllers.produtcDtail);
-
-module.exports=router;
