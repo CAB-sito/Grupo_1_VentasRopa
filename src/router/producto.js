32ad319a969d9c7fa85ad5ba78ac8cb6adfe79ba
Agustina Castillo <agustinacastillo919@gmail.com>
2024-03-09 19:59:09 -0300

    comienzo de express validator

--- src/router/producto.js
+++ src/router/producto.js
@@ -4,6 +4,14 @@ const router = express.Router();
 const multer = require("multer");
 const path = require("path");
 const authMiddleware = require("../middlewares/authMiddleware");
+const {body} = require('express-validator');
+
+const validacionesProductos = [
+  body('name').notEmpty().withMessage("Campo obligatorio")
+  .isLength({ min:5 }).withMessage("Debe tener como mínimo 5 caracteres").bail(),
+  body('description').isLength({ min:20 }).withMessage("Debe tener como mínimo 20 caracteres").bail()
+  //faltan imágenes, colores y talles (estos últimos 2 no estan en la base de datos)
+]
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -24,9 +32,9 @@ router.get("/create",authMiddleware, controller.crear);
 router.get("/detail/:id", controller.detail);
 router.get("/:id/edit", authMiddleware ,controller.modificar);
 // Añadir ruta post /
-router.post("/", update.single("imagen"), controller.guardarProducto)
+router.post("/", update.single("imagen"), validacionesProductos, controller.guardarProducto)
 // Añadir ruta put /:id
-router.put("/:id", update.single("image"), controller.editar)
+router.put("/:id", update.single("image"), validacionesProductos, controller.editar)
 // Añadir ruta delete /:id
 router.delete("/:id", controller.eliminar)
 
