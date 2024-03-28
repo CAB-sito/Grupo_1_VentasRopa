6355e103d0d761c17dc711d2b5131a5e8442abaa
gabrielcasali <gabriel9casali@gmail.com>
2024-03-28 19:54:30 -0300

    apiproducto

--- src/router/api.js
+++ src/router/api.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
+const productoController = require("../controllers/productoController");
 
+//api usuario
 router.get("/users", usersController.listar);
-
 router.get("/users/:id", usersController.detallesUsuario);
+
+//api producto 
+router.get("/products", productoController.listar);
+router.get("/products/:id", productoController.detail);
+router.post("/products", productoController.crear);
+router.put("/products/:id", productoController.modificar);
+router.delete("/products/:id", productoController.eliminar);
+
 
 module.exports = router;
