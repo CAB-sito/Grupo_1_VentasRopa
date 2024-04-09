59a977ebc4cd419832dbdb7d2e84877431a2cdc6
Cecilia <cast.cv@gmail.com>
2024-04-09 13:32:47 -0300

    pagina-preguntasFrec

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -11,21 +11,28 @@ const controller = {
   home: (req, res) => {
     //productos = db.Producto.findAll({include:["CategoriaProducto"]});
     db.Producto.findAll({
-      include:["CategoriaProducto"]
-    })
-      .then((productos)=>{
-        const productosDestacados = productos.filter((product) => product.id_categoria == 1);
-        const ofertas = productos.filter((product) => product.id_categoria == 2)
-        
-        res.render("index", {productosDestacados,ofertas, usuario: req.session.usuario})
-      })
-    
+      include: ["CategoriaProducto"],
+    }).then((productos) => {
+      const productosDestacados = productos.filter(
+        (product) => product.id_categoria == 1
+      );
+      const ofertas = productos.filter((product) => product.id_categoria == 2);
+
+      res.render("index", {
+        productosDestacados,
+        ofertas,
+        usuario: req.session.usuario,
+      });
+    });
   },
   register: (req, res) => {
     res.render("registro", { usuario: req.session.usuario });
   },
   login: (req, res) => {
     res.render(`login`, { usuario: req.session.usuario });
+  },
+  pregFrec: (req, res) => {
+    res.render(`pregFrec`, { usuario: req.session.usuario });
   },
 };
 module.exports = controller;
